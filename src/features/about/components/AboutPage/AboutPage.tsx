ae517bb734f5621c11375a0c704f17668f00db17
Rob Kennedy <robkennedy@outlook.ie>
2025-09-03 03:43:52 +0100

    Local read md file

--- src/features/about/components/AboutPage/AboutPage.tsx
+++ src/features/about/components/AboutPage/AboutPage.tsx
@@ -4,6 +4,7 @@ import { LoadingBox } from '@common/components/LoadingBox/LoadingBox';
 import { MessageBox } from '@common/components/MessageBox/MessageBox';
 import { useAboutMarkupQuery } from '@features/about/api/useAboutMarkupQuery';
 import { useTranslation } from 'react-i18next';
+import aboutText from "@assets/docs/about.md?raw";
 
 export function AboutPage() {
     const { data, isPending, isSuccess, isError } = useAboutMarkupQuery();
@@ -16,7 +17,7 @@ export function AboutPage() {
         content = <LoadingBox message={t('about.loading')} />;
     } else if (isSuccess) {
         if (data) {
-            content = <ReactMarkdown children={data} />;
+            content = <ReactMarkdown children={aboutText} />;
         }
     }
 
